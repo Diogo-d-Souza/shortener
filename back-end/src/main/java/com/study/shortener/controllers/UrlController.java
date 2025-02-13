726e2827126f481c026b290db638767e38057463
Diogo-d-Souza <di_smgomes@hotmail.com>
2025-02-13 14:08:30 -0300

    feat: simple frontend created

--- back-end/src/main/java/com/study/shortener/controllers/UrlController.java
+++ back-end/src/main/java/com/study/shortener/controllers/UrlController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class UrlController {
 
     private final UrlService urlService;
